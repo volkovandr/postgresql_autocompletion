51a6f756bed3a2ccf02085b0d9135edb24b3bfe9
unknown <andrey.v.volkov@gmail.com>
2016-07-14 21:34:05 +0200

    Added a function to the test1 that will be later tested in a unit test

--- test1.py
+++ test1.py
@@ -1,1 +1,13 @@
+'''A module used to test the functionality of the environment
+
+It prints "test" to the console
+and also has a function called aFunctionToTest that simply returnes a value
+"SomeValue"
+'''
+
 print("test")
+
+
+def aFunctionToTest():
+    '''Returns a string "SomeValue"'''
+    return "SomeValue"
