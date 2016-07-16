59ff30bd3d6a8d17146dfc34488990be8f0f8f6e
volkovandr <andrey.v.volkov@gmail.com>
2016-07-16 16:31:19 +0200

    Added unit tests that check if the modules can be imported

--- unit_test/sublime_interaction_test.py
+++ unit_test/sublime_interaction_test.py
@@ -16,3 +16,19 @@ class sublime_interaction(unittest.TestCase):
         '''Tests that the class postgresql_autocompletion can be loaded'''
         pa = postgresql_autocompletion.postgresql_autocompletion()
         self.assertIsNotNone(pa)
+
+    def testCanImportSublimeMocker(self):
+        '''Test the module sublime_mocker can be imported'''
+        import sublime_mocker
+        self.assertIsNotNone(sublime_mocker)
+
+    def testCanImportSublimePluginMocker(self):
+        '''Test the module sublime_plugin_mocker can be imported'''
+        import sublime_plugin_mocker
+        self.assertIsNotNone(sublime_plugin_mocker)
+
+#    def testReturnsNothingOnNonPostgreSQLSyntax(self):
+#        '''The plugin returns nothing when the file being edited
+#        has other syntax than PostgreSQL'''
+#        raise Exception("Not implemented")
+
