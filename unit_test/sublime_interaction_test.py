9dbb5e9e8178620dcdc7e86454e53e21543f5190
volkovandr <andrey.v.volkov@gmail.com>
2016-08-11 23:02:29 +0200

    Added unit test for sublime interaction: getQueryText()

--- unit_test/sublime_interaction_test.py
+++ unit_test/sublime_interaction_test.py
@@ -2,6 +2,8 @@
 
 import postgresql_autocompletion
 import unittest
+from sublime_mocker import view
+from sublime_mocker import selection
 
 
 class sublime_interaction(unittest.TestCase):
@@ -9,10 +11,18 @@ class sublime_interaction(unittest.TestCase):
 
     def testCheckSyntax(self):
         '''CheckSyntax works'''
-        from sublime_mocker import view
         v = view.View({'syntax': 'Packages/PostgreSQL Syntax Highlighting/\
 PostgreSQL.tmLanguage'})
         pa = postgresql_autocompletion.postgresql_autocompletion()
         self.assertEqual(pa.checkSyntax(v), True)
         v = view.View({'syntax': 'something else'})
         self.assertEqual(pa.checkSyntax(v), False)
+
+    def testGetQueryText(self):
+        '''getQueryText works'''
+        v = view.View()
+        v.set_text("This is the first query; This is the second query; /\
+This is the third query")
+        v.add_selection(selection.Selection(8, 8))
+        pa = postgresql_autocompletion.postgresql_autocompletion()
+        self.assertEqual(pa.getQueryText(v), "This is the first query")
