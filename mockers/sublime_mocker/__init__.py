326b966bbcc73775ef83c2d735bbf20f4340de38
volkovandr <andrey.v.volkov@gmail.com>
2016-08-19 21:43:07 +0200

    Revert "Added code to mock the sublime.Settings class, to load the settings from a file"
    This reverts commit 5d94f8b3a8b093e610004d06580ee3dafc80288c.

--- mockers/sublime_mocker/__init__.py
+++ mockers/sublime_mocker/__init__.py
@@ -9,7 +9,8 @@ __all__ = ["view", "selection", "region", "settings"]
 def load_settings(base_name):
     settings_file = os.path.dirname(os.path.abspath(__file__)) + "\\..\\" + \
         base_name
-    with open(settings_file) as json_file:
-        json_data = json.load(json_file)
+    json_file = open(settings_file)
+    json_data = json.load('{"something":"nothing"}')
     print(json_data)
+    json_file.close()
     return Settings(json_data)
