bb9858ed7383cee2662865ed10ede4995dfc4806
volkovandr <andrey.v.volkov@gmail.com>
2016-08-19 23:03:02 +0200

    Started with implementation of SQL parser

--- postgresql_autocompletion_lib/sqlparser.py
+++ postgresql_autocompletion_lib/sqlparser.py
@@ -2,5 +2,15 @@
 
 
 def parseSQL(query_text):
+    '''Parses a given SQL query and returns a dictionary of the elements of it.
+    Example: given query is SELECT a, b, c FROM table1 WHERE a>b
+    Returned value is
+    {
+        "select": ("a, b, c", 8, 14),
+        "from": ("table1", 21, 26),
+        "where": ("a>b", 34, 36)
+    }
+    '''
     if query_text is None or len(query_text) == 0:
         return None
+
--- unit_test/sqlparser_test.py
+++ unit_test/sqlparser_test.py
@@ -9,3 +9,8 @@ class basic_parser_tests(unittest.TestCase):
 
     def testEmptyString(self):
         self.assertIsNone(sqlparser.parseSQL(''))
+
+    def testSelectList(self):
+        query_text = "SELECT a, b, c FROM table"
+        parsed = sqlparser.parseSQL(query_text)
+        self.assertEqual(parsed["Select-List"],("a, b, c", 8, 14))
