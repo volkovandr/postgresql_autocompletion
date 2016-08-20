c1bef4dee3f3e1a255092ddb03425924053aebda
volkovandr <andrey.v.volkov@gmail.com>
2016-08-20 14:39:44 +0200

    Added a pyparsing module to the project

--- postgresql_autocompletion_lib/sqlparser.py
+++ postgresql_autocompletion_lib/sqlparser.py
@@ -1,7 +1,9 @@
 '''Implementation of an SQL parser used by the plugin'''
 
+from pyparsing.pyparsing import *
 
-def parseSQL(query_text):
+
+def base_parse(query_text):
     '''Parses a given SQL query and returns a dictionary of the elements of it.
     Example: given query is SELECT a, b, c FROM table1 WHERE a>b
     Returned value is
@@ -9,7 +11,22 @@ def parseSQL(query_text):
         "select": ("a, b, c", 8, 14),
         "from": ("table1", 21, 26),
         "where": ("a>b", 34, 36)
-    }
+    } and so on: the contents of every block of a query it could find.
+    Subqueries are returned as is.
+    Returns None on empty string or None
+    The expected structure of an SQL query is the following:
+    [something] [SELECT <select block>] [FROM <from block>]
+    [WHERE <where block>] [GROUP BY <group by block]
+    [HAVING <having block>] [WINDOW <window block>]
+    [UNION|EXCEPT|INTERSECT [ALL|DISTINCT]]
+    [ORDER BY <order by block>]
+    [LIMIT <limit block>]
+    [OFFSET <offset block>]
+    Blocks are separated from each other by whitespace, newline or
+    closing parethesis.
+    Blocks' contents are separated from the keywords by whitespace, newline or
+    opening parenthesis.
+    Comments are ignored.
     '''
     if query_text is None or len(query_text) == 0:
         return None
--- unit_test/sqlparser_test.py
+++ unit_test/sqlparser_test.py
@@ -8,9 +8,10 @@ from postgresql_autocompletion_lib import sqlparser
 class basic_parser_tests(unittest.TestCase):
 
     def testEmptyString(self):
-        self.assertIsNone(sqlparser.parseSQL(''))
+        self.assertIsNone(sqlparser.base_parse(''))
 
     def testSelectList(self):
         query_text = "SELECT a, b, c FROM table"
-        parsed = sqlparser.parseSQL(query_text)
-        self.assertEqual(parsed["Select-List"],("a, b, c", 8, 14))
+        parsed = sqlparser.base_parse(query_text)
+        self.assertEqual(parsed["select"], ("a, b, c", 8, 14))
+
