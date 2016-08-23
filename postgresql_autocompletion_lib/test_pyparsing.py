464f74b500b91f4efcf9e1c18fa7ce9ccb2a9661
volkovandr <andrey.v.volkov@gmail.com>
2016-08-23 21:35:30 +0200

    Simplified the test_pyparsing a bit

--- postgresql_autocompletion_lib/test_pyparsing.py
+++ postgresql_autocompletion_lib/test_pyparsing.py
@@ -8,22 +8,29 @@
 from pyparsing import CaselessLiteral, Word, upcaseTokens, \
     delimitedList, Optional, Combine, Group, alphas, nums, alphanums, \
     ParseException, Forward, oneOf, quotedString, ZeroOrMore, restOfLine,\
-    Keyword
+    Keyword, originalTextFor
 
 
 def test(str):
     print(str, "->")
     try:
         tokens = simpleSQL.parseString(str)
-        print("tokens = ", tokens)
-        print("tokens.columns =", tokens.columns)
-        print("tokens.tables =", tokens.tables)
-        print("tokens.where =", tokens.where)
+        print("tokens          =", tokens)
+        print("Select clause   =", tokens.select)
+        print("Select list     =", tokens.select[1])
+        print("From clause     =", tokens.from_)
+        print("Tables          =", tokens.from_[1])
+        print("Where clause    =", tokens.where)
+        print("Where condition =", tokens.where)
     except ParseException as err:
         print(" " * err.loc + "^\n" + err.msg)
         print(err)
     print
 
+def parse_action_test(s, loc, toks):
+    print("s", s)
+    print("loc", loc)
+    print("toks", toks)
 
 # define SQL tokens
 selectStmt = Forward()
@@ -31,12 +38,11 @@ selectToken = Keyword("select", caseless=True)
 fromToken = Keyword("from", caseless=True)
 
 ident = Word(alphas, alphanums + "_$").setName("identifier")
-columnName = \
-    (delimitedList(ident, ".", combine=True)).setParseAction(upcaseTokens)
+columnName = (delimitedList(ident, ".", combine=True))
 columnNameList = Group(delimitedList(columnName))
-tableName = (delimitedList(ident, ".", combine=True)). \
-    setParseAction(upcaseTokens)
-tableNameList = Group(delimitedList(tableName))
+tableName = (delimitedList(ident, ".", combine=True))
+tableNameList = Group(delimitedList(tableName)). \
+    setParseAction(parse_action_test)
 
 whereExpression = Forward()
 and_ = Keyword("and", caseless=True)
@@ -64,12 +70,11 @@ whereCondition = Group(
 whereExpression << whereCondition + ZeroOrMore((and_ | or_) + whereExpression)
 
 # define the grammar
-selectStmt << (selectToken +
-               ('*' | columnNameList).setResultsName("columns") +
-               fromToken +
-               tableNameList.setResultsName("tables") +
-               Optional(Group(CaselessLiteral("where") +
-                              whereExpression), "").setResultsName("where"))
+selectStmt << (
+    Group(selectToken + ('*' | columnNameList)).setResultsName("select") +
+    Group(fromToken + tableNameList).setResultsName("from_") +
+    Optional(Group(CaselessLiteral("where") +
+                   whereExpression), "").setResultsName("where"))
 
 simpleSQL = selectStmt
 
@@ -78,17 +83,19 @@ oracleSqlComment = "--" + restOfLine
 simpleSQL.ignore(oracleSqlComment)
 
 
-test("SELECT * from XYZZY, ABC")
-test("select * from SYS.XYZZY")
-test("Select A from Sys.dual")
-test("Select A,B,C from Sys.dual")
-test("Select A, B, C from Sys.dual")
-test("Select A, B, C from Sys.dual, Table2   ")
-test("Xelect A, B, C from Sys.dual")
-test("Select A, B, C frox Sys.dual")
-test("Select")
-test("Select &&& frox Sys.dual")
-test("Select A from Sys.dual where a in ('RED','GREEN','BLUE')")
-test("Select A from Sys.dual where a in ('RED','GREEN','BLUE') and b in (10,20,30)")
-test("Select A,b from table1,table2 where table1.id eq table2.id -- test out comparison operators")
-test("SELECT a FROM (SELECT b) a")
+test("SELECT a, b, c from table1, table2 WHERE a = b")
+#test("select * from SYS.XYZZY")
+#test("Select A from Sys.dual")
+#test("Select A,B,C from Sys.dual")
+#test("Select A, B, C from Sys.dual")
+#test("Select A, B, C from Sys.dual, Table2   ")
+#test("Xelect A, B, C from Sys.dual")
+#test("Select A, B, C frox Sys.dual")
+# test("Select")
+#test("Select &&& frox Sys.dual")
+#test("Select A from Sys.dual where a in ('RED','GREEN','BLUE')")
+# test("Select A from Sys.dual where a in ('RED','GREEN','BLUE')" +
+#     " and b in (10,20,30)")
+# test("Select A,b from table1,table2 where table1.id eq table2.id" +
+#     " -- test out comparison operators")
+#test("SELECT a FROM (SELECT b) a")
