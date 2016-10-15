56cb61815796e38d2aae71beefba105af0a0363f
volkovandr <andrey.v.volkov@gmail.com>
2016-10-15 10:12:28 +0200

    A very nice example of how to parse "empty from elements" when the user as entered a tablename then pressed comma and nothing else or several spaces. That "nothing else" will be treated as table or schemaname

--- postgresql_autocompletion_lib/test_pyparsing.py
+++ postgresql_autocompletion_lib/test_pyparsing.py
@@ -1,45 +1,76 @@
-# simpleSQL.py
-#
-# simple demo of using the parsing library to do simple-minded SQL parsing
-# could be extended to include where clauses etc.
-#
-# Copyright (c) 2003, Paul McGuire
-#
 from pyparsing import *
 
-
-identifier = Word(alphas, alphanums).setName("identifier")
+identifier = Word(alphas + "_", alphanums + "_").setName("identifier")
 as_keyword = Keyword("as", caseless=True).setName("AS")
 
 schemaname = locatedExpr(identifier).setResultsName("schemaname")
 tablename = locatedExpr(identifier).setResultsName("tablename")
-table_or_schemaname = locatedExpr(identifier).setResultsName("table_or_schemaname")
 alias = locatedExpr(identifier).setResultsName("alias")
 
 full_qualified_table_name = schemaname + "." + tablename + \
     Optional(as_keyword) + Optional(alias)
 unqualified_table_name = tablename + Optional(as_keyword) + alias
+table_or_schemaname = \
+    locatedExpr(identifier).setResultsName("table_or_schemaname")
 
-from_element = Suppress(StringStart() | ",") + (full_qualified_table_name | unqualified_table_name | table_or_schemaname) + (FollowedBy(",") | FollowedBy(StringEnd()))
+schema_name_and_dot = schemaname + "." + Optional(as_keyword) + \
+    Optional(alias)
+
+nothing = White().setResultsName('nothing') + (FollowedBy(",") | FollowedBy(StringEnd()))
+
+
+from_element = Suppress(StringStart() | ",") + \
+    (
+        full_qualified_table_name |
+        schema_name_and_dot.setResultsName("schemaname_and_dot") |
+        unqualified_table_name |
+        table_or_schemaname | nothing) + \
+    (FollowedBy(",") | FollowedBy(StringEnd()))
 
 parse = from_element.parseWithTabs()
+
 
 def test(test_str):
     print(">>>", test_str)
     try:
         tokens = parse.scanString(test_str)
-        #print(tokens)
         for substring in tokens:
             from_element = substring[0]
             print(">>>>>", test_str[substring[1]:substring[2]])
-            if from_element.schemaname != '':
-                print("schemaname:", from_element.schemaname)
             if from_element.tablename != '':
-                print("tablename :", from_element.tablename)
+                print(
+                    "Tablename:",
+                    '"' + str(from_element.tablename[1]) + '"',
+                    str(from_element.tablename[0]) + ":" +
+                    str(from_element.tablename[2]))
             if from_element.table_or_schemaname != '':
-                print("t_or_sch  :", from_element.table_or_schemaname)
+                print(
+                    "Table- or schemaname:",
+                    '"' + str(from_element.table_or_schemaname[1]) + '"',
+                    str(from_element.table_or_schemaname[0]) + ":" +
+                    str(from_element.table_or_schemaname[2]))
             if from_element.alias != '':
-                print("alias     :", from_element.alias)
+                print(
+                    "Alias:",
+                    '"' + str(from_element.alias[1]) + '"',
+                    str(from_element.alias[0]) + ":" +
+                    str(from_element.alias[2]))
+            if from_element.schemaname != '':
+                print(
+                    "Schemaname:",
+                    '"' + str(from_element.schemaname[1]) + '"',
+                    str(from_element.schemaname[0]) + ":" +
+                    str(from_element.schemaname[2]))
+            if from_element.schemaname_and_dot != '':
+                print(
+                    "Tablename (empty):",
+                    str(from_element.schemaname[2] + 1) + ":" +
+                    str(from_element.schemaname[2] + 1))
+            if from_element.nothing != '':
+                print(
+                    "Table or schemaname (empty):",
+                    '"' + str(from_element.nothing) + '"',
+                    str(substring[1] + 1) + ":" + str(substring[2]))
     except ParseException as e:
         print(e)
     print()
@@ -47,23 +78,13 @@ def test(test_str):
 
 test_strings = [
     "",
-    "table1",
-    "table1 as t1",
-    "table1 t1",
-    "schema1.table1 as t1",
-    "schema1.table1 t1",
-    "schema1.table1",
+    "table1 as zo",
     "table1, table2",
-    "table1 as t1, table2 as t2",
-    "table1, schema2.table2 t2",
-    "schema1.table1 t1, schema2.table2 as t2",
-    "s1.t1, s2.t2, s3.t3, s4.t4",
-    "bla1 bla2 bla3 bla4",
-    '''schema1.table1
-    as zorro  ,
-    schema2.table2
-    mumba''',
-    "bla.bla, zok.mok, a b c, kum.zum"]
+    "table1,           , table2",
+    " ,      ",
+    "table, ",
+    "bla bla bla",
+    "mok."]
 
 for test_str in test_strings:
     test(test_str)
